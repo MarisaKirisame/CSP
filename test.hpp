3b7449c2044d97d62f5e19ec9a206cc66f498eb8
lolisa <lolisa@marisa.moe>
2015-09-03 18:05:10 -0700

    remove extra include line.

--- test.hpp
+++ test.hpp
@@ -3,7 +3,6 @@
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
-#include <boost/test/unit_test.hpp>
 using namespace CSP;
 BOOST_AUTO_TEST_CASE( CSP_test )
 {
