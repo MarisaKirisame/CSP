30bcc984114069b3af1d672d47b255623cdebfc6
lolisa <lolisa@marisa.moe>
2015-11-15 02:24:24 -0800

    fix clang compile error

--- test.hpp
+++ test.hpp
@@ -271,5 +271,7 @@ BOOST_AUTO_TEST_CASE( CSP_test )
         BOOST_CHECK( result.size( ) == 1 );
     }
 }
-
+namespace boost { namespace unit_test { namespace ut_detail {
+    std::string normalize_test_case_name(const_string name) {
+        return ( name[0] == '&' ? std::string(name.begin()+1, name.size()-1) : std::string(name.begin(), name.size() )); }}}}
 #endif // TEST_HPP
